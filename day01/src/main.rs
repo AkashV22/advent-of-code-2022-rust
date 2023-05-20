2db84242fadbda09751c52b5230c28f8e7bb7af4
AkashV22 <iakashv22@gmail.com>
2023-05-20 09:33:25 +0100

    Tweaked my day 1 exercise solution a bit and ran cargo fmt.

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -4,33 +4,38 @@ use std::{
     path::PathBuf,
 };
 
-fn get_total_calories_for_elf_with_most_calories(input_file: &str) -> Result<u32, Box<dyn std::error::Error>> {
+fn get_total_calories_for_elf_with_most_calories(input_file: &str) -> Result<u32, String> {
     let input_path: PathBuf = [env!("CARGO_MANIFEST_DIR"), input_file].iter().collect();
 
-    let file = File::open(input_path)?;
+    let file = File::open(input_path).map_err(|e| format!("{:?}", e))?;
     let buf = BufReader::new(file);
 
     let mut max_calories: u32 = 0;
     let mut calories_for_current_elf: u32 = 0;
     for line in buf.lines() {
-        let calories_str: &str = &line?;
-        if calories_str.is_empty() {
-            if calories_for_current_elf > max_calories {
-                max_calories = calories_for_current_elf;
-            }
-            calories_for_current_elf = 0;
-        } else {
-            calories_for_current_elf += calories_str.parse::<u32>()?;
+        let calories_str: &str = &line.map_err(|e| format!("{:?}", e))?;
+        if !calories_str.is_empty() {
+            calories_for_current_elf += calories_str
+                .parse::<u32>()
+                .map_err(|e| format!("{:?}", e))?;
+            continue;
         }
+        if calories_for_current_elf > max_calories {
+            max_calories = calories_for_current_elf;
+        }
+        calories_for_current_elf = 0;
     }
 
     Ok(max_calories)
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> Result<(), String> {
     let max_calories: u32 = get_total_calories_for_elf_with_most_calories("input.txt")?;
 
-    println!("The elf carrying the most calories is carrying {} calories in total!", max_calories);
+    println!(
+        "The elf carrying the most calories is carrying {} calories in total!",
+        max_calories
+    );
 
     Ok(())
 }
@@ -41,6 +46,9 @@ mod tests {
 
     #[test]
     fn test_using_example_input() {
-        assert_eq!(Ok(24000), get_total_calories_for_elf_with_most_calories("example-input.txt").map_err(|e| format!("{:?}", e)));
+        assert_eq!(
+            Ok(24000),
+            get_total_calories_for_elf_with_most_calories("example-input.txt")
+        );
     }
 }
