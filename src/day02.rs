d9132309f8b424221c134d7b44e5de4062a4d08c
AkashV22 <iakashv22@gmail.com>
2023-05-20 14:49:54 +0100

    Implemented the solution for day 1 puzzle 2 a bit more.

--- src/day02.rs
+++ src/day02.rs
@@ -1,3 +1,10 @@
+use std::{
+    fs::File,
+    io,
+    io::{BufRead, BufReader},
+    path::PathBuf,
+};
+
 enum Move {
     Rock,
     Paper,
@@ -5,22 +12,6 @@ enum Move {
 }
 
 impl Move {
-    fn opponent(&self) -> char {
-        match self {
-            Move::Rock => 'A',
-            Move::Paper => 'B',
-            Move::Scissors => 'C',
-        }
-    }
-
-    fn player(&self) -> char {
-        match self {
-            Move::Rock => 'X',
-            Move::Paper => 'Y',
-            Move::Scissors => 'Z',
-        }
-    }
-
     fn score(&self) -> u8 {
         match self {
             Move::Rock => 1,
@@ -30,9 +21,36 @@ impl Move {
     }
 }
 
+fn opponent_key_to_move(key: char) -> Option<Move> {
+    match key {
+        'A' => Some(Move::Rock),
+        'B' => Some(Move::Paper),
+        'C' => Some(Move::Scissors),
+        _ => None,
+    }
+}
+
+fn player_key_to_move(key: char) -> Option<Move> {
+    match key {
+        'X' => Some(Move::Rock),
+        'Y' => Some(Move::Paper),
+        'Z' => Some(Move::Scissors),
+        _ => None,
+    }
+}
+
 /// Puzzle 1
-pub(super) fn get_total_score_after_rps_games(input_file: &str) -> u32 {
-    15
+pub(super) fn get_total_score_after_rps_games(input_file: &str) -> Result<u32, io::Error> {
+    let input_path: PathBuf = [env!("CARGO_MANIFEST_DIR"), "input", input_file]
+        .iter()
+        .collect();
+
+    let file = File::open(input_path)?;
+    let buf = BufReader::new(file);
+
+    for line in buf.lines() {}
+
+    Ok(15)
 }
 
 #[cfg(test)]
@@ -42,8 +60,9 @@ mod tests {
     #[test]
     fn get_total_score_after_rps_games_using_example_input() {
         assert_eq!(
-            15,
+            Ok(15),
             get_total_score_after_rps_games("day02-example-input.txt")
+                .map_err(|e| format!("{:?}", e))
         );
     }
 }
